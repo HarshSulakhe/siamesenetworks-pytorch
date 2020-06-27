a822e0e5f44cb27a983ed462b94733102e73e4c5
HarshSulakhe <sulakheharsh@gmail.com>
2020-06-28 04:20:04 +0530

    Modify model to give single output

--- model.py
+++ model.py
@@ -2,52 +2,39 @@ import torch.nn as nn
 
 
 class SiameseNetwork(nn.Module):
-     def __init__(self):
-          super(SiameseNetwork, self).__init__()
-          # Setting up the Sequential of CNN Layers
-          self.cnn1 = nn.Sequential(
-          nn.Conv2d(1, 96, kernel_size=11,stride=1),
-          nn.ReLU(inplace=True),
-          nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
-          nn.MaxPool2d(3, stride=2),
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        self.cnn1 = nn.Sequential(
+        nn.Conv2d(3,64,5),
+        nn.ReLU(True),
+        nn.MaxPool2d(2,2),
+        # Currently 110x110x64
+        nn.Conv2d(64,96,3),
+        nn.ReLU(True),
+        nn.MaxPool2d(2,2),
+        #Currently 54x54x96
+        nn.Conv2d(96,128,3),
+        nn.ReLU(True),
+        nn.MaxPool2d(2,2),
+        #Currently 26x26x128
+        nn.Conv2d(128,128,3),
+        nn.ReLU(True),
+        nn.MaxPool2d(2,2)
+        #Currently 12x12x128
+        )
 
-          nn.Conv2d(96, 256, kernel_size=5,stride=1,padding=2),
-          nn.ReLU(inplace=True),
-          nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
-          nn.MaxPool2d(3, stride=2),
-          nn.Dropout2d(p=0.3),
+        self.fc1 = nn.Sequential(
+        nn.Linear(18432 , 1024),
+        nn.Dropout(),
+        nn.Linear(1024,128))
 
-          nn.Conv2d(256,384 , kernel_size=3,stride=1,padding=1),
-          nn.ReLU(inplace=True),
-          nn.Conv2d(384,256 , kernel_size=3,stride=1,padding=1),
-          nn.ReLU(inplace=True),
-          nn.MaxPool2d(3, stride=2),
-          nn.Dropout2d(p=0.3),
-          )
+    def forward_once(self, x):
+        output = self.cnn1(x)
+        output = output.view(-1,18432)
+        output = self.fc1(output)
+        return output
 
-          # Defining the fully connected layers
-          self.fc1 = nn.Sequential(
-          # First Dense Layer
-          nn.Linear(30976, 1024),
-          nn.ReLU(inplace=True),
-          nn.Dropout2d(p=0.5),
-          # Second Dense Layer
-          nn.Linear(1024, 128),
-          nn.ReLU(inplace=True),
-          # Final Dense Layer
-          nn.Linear(128,2))
-
-     def forward_once(self, x):
-          # Forward pass
-          output = self.cnn1(x)
-          output = output.view(output.size()[0], -1)
-          output = self.fc1(output)
-          return output
-
-    def forward(self, input1, input2):
-         # forward pass of input 1
-         output1 = self.forward_once(input1)
-         # forward pass of input 2
-         output2 = self.forward_once(input2)
-         # returning the feature vectors of two inputs
-         return output1, output2
+    def forward(self, input1):
+        output1 = self.forward_once(input1)
+        # output2 = self.forward_once(input2)
+        return output1
