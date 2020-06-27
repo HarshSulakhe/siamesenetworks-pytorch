44d146022579ac1ff8f869780e05d7df44fa949a
HarshSulakhe <sulakheharsh@gmail.com>
2020-06-28 04:20:41 +0530

    Modify loss

--- loss.py
+++ loss.py
@@ -9,5 +9,7 @@ class ContrastiveLoss(torch.nn.Module):
 
     def forward(self, output1, output2, label):
         euclidean_distance = F.pairwise_distance(output1, output2)
-        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) + (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        pos = (1-label) * torch.pow(euclidean_distance, 2)
+        neg = (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)
+        loss_contrastive = torch.mean( pos + neg )
         return loss_contrastive
